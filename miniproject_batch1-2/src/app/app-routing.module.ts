26477cf0e17d71182b4a2fdf2d5a2c87e3a4c1f0
haripriya s <haripriyas4324@gmail.com>
2023-02-12 20:12:23 +0530

    initial

--- miniproject_batch1-2/src/app/app-routing.module.ts
+++ miniproject_batch1-2/src/app/app-routing.module.ts
@@ -1,23 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContactComponent } from './contact/contact.component';
-import {HomeComponent} from './home/home.component';
-import { DepartmentComponent } from './department/department.component';
 
 
 const routes: Routes = [
-  {
-    path:"home",component:HomeComponent
-  },
-  {
-    path:"",component:HomeComponent
-  },
-  {
-    path:"contact",component:ContactComponent
-  },
-  {
-    path:"department",component:DepartmentComponent
-  }
+ 
 ];
 
 @NgModule({
